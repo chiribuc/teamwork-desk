eaa1eaf1b56e690d6b450a152125c295c4c8dd5a
Robert-Cristian Chiribuc <me@crobert.ro>
2022-09-29 19:21:28 +0300

    create test fixtures
    Signed-off-by: Robert-Cristian Chiribuc <me@crobert.ro>

---
+++ tests/Fixtures/AuthenticatedUser.php
@@ -0,0 +1,58 @@
+<?php
+
+function me(): array
+{
+    return [
+        "user"               => [
+            "id"                       => 205445,
+            "email"                    => "me@crobert.ro",
+            "firstName"                => "Technical",
+            "lastName"                 => "Support",
+            "avatarURL"                => "https://s3.amazonaws.com/TWFiles/491882/userAvatar/7b755f58-5g86-9e5q-a167-40ea4ca1428d.png",
+            "editMethod"               => "html",
+            "isPartTime"               => false,
+            "onboarding"               => [
+                "dismissedWelcome"        => true,
+                "exploreReports"          => true,
+                "exploreHelpsites"        => true,
+                "checkDeskHelpSite"       => true,
+                "checkSampleInbox"        => false,
+                "reviewedProfile"         => true,
+                "reviewedCompanySettings" => true,
+                "repliedToFirstTicket"    => true,
+                "bookmarkPage"            => false,
+                "addedFirstInbox"         => true,
+                "inviteTeam"              => true,
+            ],
+            "ticketReplyRedirect"      => "Inbox",
+            "reviewer"                 => false,
+            "trainingWheelsEnrollment" => null,
+            "role"                     => "Admin",
+            "sendPushNotifications"    => true,
+            "sendWebNotifications"     => false,
+            "timeFormatId"             => 1,
+            "timezoneId"               => 62,
+            "projectsCompanyId"        => 101554,
+            "isAppOwner"               => true,
+            "ldKey"                    => "77cca1ebb246ae7516fa4b5a4766c84e85847b0e0d69841ec39ffa2289492f31",
+            "createdAt"                => "2018-07-02T08:30:09Z",
+            "updatedAt"                => "2022-09-17T22:06:58Z",
+            "createdBy"                => [
+                "id"   => 999999999,
+                "type" => "users",
+            ],
+            "updatedBy"                => [
+                "id"   => 205154,
+                "type" => "users",
+            ],
+            "state"                    => "active",
+        ],
+        "websocketAuthToken" => [
+            "hash"            => "c02a10129857b57692c3551234cf20cbbf3513cd",
+            "installation_id" => 458885,
+            "user_id"         => 254569,
+            "timestamp"       => "2022-09-17 22:06:58 +0000 UTC",
+        ],
+        "included"           => [],
+    ];
+}
