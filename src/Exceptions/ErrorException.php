27158ad73ed3ccd4fb5feb48eede905366e85d8f
Robert-Cristian Chiribuc <me@crobert.ro>
2022-09-29 19:22:36 +0300

    [Resources\*] add optional parameters
    Signed-off-by: Robert-Cristian Chiribuc <me@crobert.ro>

--- src/Exceptions/ErrorException.php
+++ src/Exceptions/ErrorException.php
@@ -11,11 +11,11 @@ final class ErrorException extends Exception
     /**
      * Creates a new Exception instance.
      *
-     * @param array{message: string, type: string, code: string} $contents
+     * @param array{id: string, title: string, code: string, detail: string} $contents
      */
     public function __construct(private readonly array $contents)
     {
-        parent::__construct($contents['message']);
+        parent::__construct($contents['title']);
     }
 
     /**
@@ -27,11 +27,19 @@ final class ErrorException extends Exception
     }
 
     /**
-     * Returns the error type.
+     * Returns the error ID.
      */
-    public function getErrorType(): string
+    public function getErrorId(): string
     {
-        return $this->contents['type'];
+        return $this->contents['id'];
+    }
+
+    /**
+     * Returns the error title.
+     */
+    public function getErrorTitle(): string
+    {
+        return $this->contents['title'];
     }
 
     /**
@@ -40,5 +48,13 @@ final class ErrorException extends Exception
     public function getErrorCode(): string
     {
         return $this->contents['code'];
+    }
+
+    /**
+     * Returns the error detail.
+     */
+    public function getErrorDetail(): string
+    {
+        return $this->contents['detail'];
     }
 }
